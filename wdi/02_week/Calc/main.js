772cd9792e44ee67a6127f4eff2a5c5cb10324dc
ashlopez <YOUR-ashleylopezglobal@gmail.com>
2014-11-24 16:01:01 -0800

    calc

---
+++ wdi/02_week/Calc/main.js
@@ -0,0 +1,13 @@
+
+var AreaBox= function(length,width) {   
+    return length* width;
+};
+var x= prompt("What is the length of the line?")
+		
+
+var y= prompt("What is the width of the line?")
+
+
+
+AreaBox(x,y)
+
